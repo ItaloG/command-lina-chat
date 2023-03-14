a134591de82cfb1a33a201fae88f4b11649b1cfd
Italo Gabriel <Italog03@gmail.com>
2023-03-13 21:46:32 -0300

    feat: application interface

---
+++ hacker-chat/client/index.js
@@ -0,0 +1,7 @@
+import Events from "node:events";
+import TerminalController from "./src/terminalController.js";
+
+const componentEmitter = new Events();
+
+const controller = new TerminalController();
+await controller.initializeTable(componentEmitter);
