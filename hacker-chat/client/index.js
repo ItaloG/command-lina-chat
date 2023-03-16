a8311262ef2c9c02918c123994445311af48ff71
Italo Gabriel <Italog03@gmail.com>
2023-03-15 21:24:58 -0300

    feat: create socket server and connection

--- hacker-chat/client/index.js
+++ hacker-chat/client/index.js
@@ -1,7 +1,22 @@
+/*
+node index.js \
+    --username italoG \
+    --room sala01 \
+    --hostUri localhost
+*/
+
 import Events from "node:events";
 import TerminalController from "./src/terminalController.js";
+import CliConfig from "./src/cliConfig.js";
+import SocketClient from "./src/socket.js";
+
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
 
 const componentEmitter = new Events();
+const socketClient = new SocketClient(config);
 
-const controller = new TerminalController();
-await controller.initializeTable(componentEmitter);
+await socketClient.initialize();
+
+// const controller = new TerminalController();
+// await controller.initializeTable(componentEmitter);
---
+++ hacker-chat/server/src/index.js
@@ -0,0 +1,17 @@
+import { constants } from "./constants.js";
+import Controller from "./controller.js";
+import SocketServer from "./socket.js";
+import Event from "node:events";
+
+const eventEmitter = new Event();
+
+const port = process.env.PORT || 9898;
+const socketServer = new SocketServer({ port });
+const server = await socketServer.initialize(eventEmitter);
+console.log("socket server is running at", server.address().port);
+
+const controller = new Controller({ socketServer });
+eventEmitter.on(
+  constants.event.NEW_USER_CONNECTED,
+  controller.onNewConnection.bind(controller)
+);
